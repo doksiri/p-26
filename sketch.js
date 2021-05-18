e62148cd1cc2d12d5a08a70824dec7e6ea8660d6
doksiri <payengrepublica@gmail.com>
2021-05-18 21:28:04 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,23 @@
+Khorisa Maas recipe :
+
+Ingredients:
+
+• 5-6 pieces of Rahu fish (one may use other fishes too)
+• About 100 gm grated bamboo shoots (pickled, fermented or raw)
+• Two-three tablespoon oil depending on the type of pan you’re using
+• One-two medium-sized tomato (cut into small pieces)
+• Two green chillies
+• 1/4th teaspoon white mustard seeds
+• Salt to taste
+
+Method:
+
+Marinate the fish pieces with turmeric, salt and a dash of oil for half an hour/one hour. Pour the oil in a 
+pan and fry the fish pieces (not too deep). Set them aside. A lot of people use ginger garlic paste in this 
+preparation but since i prefers the undiluted taste of khorisa, I did not use any. In the remaining oil
+ (shouldn’t be more than one teaspoon), add the mustard seeds. When they splutter, add the tomatoes. Add the 
+ salt, turmeric, and green chillies. In the mean time, mix the khorisa (I have used khorisa pickle) with some
+  water, and set aside One may even use the grated bamboo shoots in raw or fermented form. However your
+  cooking procedure will differ depending on how you’re using the khorisa When the tomatoes go pulpy, add hot 
+  water. As the gravy thickens a bit, add the fish pieces. Soon add the khorisa mixture to the preparation. 
+  Heat for some time and your ‘khorisa maas’ is ready.
